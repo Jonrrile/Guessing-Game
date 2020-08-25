94b66b73eca63af556ee3d2045b71df35ca32e5c
Jon Riley <jonrrile@ut.utm.edu>
2020-08-25 14:17:03 -0500

    Up to phase 5

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's play a game... Guess the number?");
+            string answer = Console.ReadLine();
+            string correctNumber = "42";
+            // Console.WriteLine($"You guessed {answer}.. that is incorrect!");
+            if (answer == correctNumber)
+            {
+                Console.Write("You are correct!");
+            }
+            else
+            {
+                Console.WriteLine("Try again?");
+                answer = Console.ReadLine();
+            }
+
+        }
+    }
+}
