3f494a8ee6f5611a27ca49458d3239f874db3a13
Jon Riley <jonrrile@ut.utm.edu>
2020-09-08 20:31:25 -0500

    Working on some C# practice from earlier chapters.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Guessing_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Guessing Game.");
+            Console.WriteLine("------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("Please enter your guess.");
+
+            string guess = Console.ReadLine();
+            Console.WriteLine($"Your guess was {guess}.");
+
+        }
+    }
+}
